87c0ed4910fdb02e7d719bdf5194a83d7486fee7
TuCoSan <95779657+TuCoSan@users.noreply.github.com>
2023-06-28 22:51:15 +0000

    now we have the language option!

--- lxfspacy.py
+++ lxfspacy.py
@@ -1,19 +1,29 @@
-# user needs to choose model language 
+import spacy
 
-
-import spacy 
-
-def nlp(str):
-  return spacy.load(str)
+language_dict = {
+  "pt": {
+    "speed": "pt_core_news_sm",
+    "accuracy": "pt_core_news_lg"
+  },
+  "en": {
+   "speed": "en_core_web_sm",
+   "accuracy": "en_core_web_trf"
+  },
+  "ru": {
+    "speed": "ru_core_news_sm",
+    "accuracy": "ru_core_news_lg"
+  }
+}
+ 
+def nlp(lang, opt):
+  return spacy.load(language_dict[lang][opt])
 
 #function getLexemes
-def getLexemes(txt):
+def getLexemes(txt, nlp):
   renderedText = nlp(txt)
   lexemeList = []
   for token in renderedText:
     if token.is_alpha:
       lexemeList.append(token.lemma_)
   return(lexemeList)
-
-
 

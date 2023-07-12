935563d5b708452221dc298c5827d38cbc31e77c
TuCoSan <95779657+TuCoSan@users.noreply.github.com>
2023-07-12 22:48:26 +0000

    Function to install and download the language package

--- lxfspacy.py
+++ lxfspacy.py
@@ -27,3 +27,11 @@ def getLexemes(txt, nlp):
       lexemeList.append(token.lemma_)
   return(lexemeList)
 
+def install_language_package(package_name):
+    try:
+        spacy.cli.download(package_name)
+        print(f"Successfully installed {package_name} language package.")
+    except Exception as e:
+        print(f"Error installing {package_name} language package: {str(e)}")
+
+  
